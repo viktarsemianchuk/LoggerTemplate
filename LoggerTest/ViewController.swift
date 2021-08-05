b58c33241984a31729f47cda65e2fe283624ecbe
Uladzislau Stankevich <rct.stankevivs@gmail.com>
2021-08-05 17:16:41 +0300

    Initial Commit

---
+++ LoggerTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoggerTest
+//
+//  Created by Владислав Станкевич on 5.08.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
